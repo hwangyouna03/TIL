a5c5166872f2b712ca0e4b76f9033b1f39697afe
Yuna Hwang <57125986+hwangyouna03@users.noreply.github.com>
2021-04-26 15:08:51 +0900

    Add files via upload

---
+++ CodingTest_JS/Basic.js
@@ -0,0 +1,46 @@
+console.log('Hello World!');
+
+//두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+console.log('강한친구 대한육군 강한친구 대한육군');
+
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램
+// var A = prompt('A 입력: ');
+// var B = prompt('B 입력: ');
+// //string 타입을 정수로 변환
+// A = Number(A);
+// B = Number(B);
+// console.log('A+B = ', A+B);
+
+//두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+// let c = prompt('c: ');
+// let d = prompt('d: ');
+// c = Number(c);
+// d = Number(d);
+// console.log('c-d=', c-d);
+
+//10998. 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+// let a = prompt('a: ');
+// let b = prompt('b: ');
+// a = Number(a);
+// b = Number(b);
+// console.log('a*b=', a*b);
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+// let A = prompt('A: ');
+// let B = prompt('B: ');
+// A = Number(A);
+// B = Number(B);
+// console.log('A/B=', A/B);
+
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+A = prompt('A: ');
+B = prompt('B: ');
+A = Number(A);
+B = Number(B);
+console.log('A%B=',A%B);
+
+
+//(세자리수)*(세자리수)
+//https://www.acmicpc.net/problem/2588
+let num1 = 472;
+let num2 = 385;
+console.log('1)', num1*num2);
